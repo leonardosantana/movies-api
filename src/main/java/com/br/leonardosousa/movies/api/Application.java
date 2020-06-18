3fcf8efe3c8f1bc4bc174caca335e4cf01e2316b
leonardo.sousa@cabal.com.br <leozin.santana@gmail.com>
2020-06-17 21:41:40 -0300

    override just dev properties

--- src/main/java/com/br/leonardosousa/movies/api/Application.java
+++ src/main/java/com/br/leonardosousa/movies/api/Application.java
@@ -1,13 +1,19 @@
 package com.br.leonardosousa.movies.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+	@Value("${welcome.message}")
+	public String welcomeMessage;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+
 	}
 
 }
